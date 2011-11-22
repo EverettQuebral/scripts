4f453fd0bcdeda2293f9c3be5d400fe19cb80819
Everett Quebral <equebral@paypal.com>
2011-11-22 07:51:14 -0800

    first commit

---
+++ remoteMobileWebInspector.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# author : Everett P. Quebral
+# license : MIT License
+
+# this script will let you remotely debug your page in Mobile Safari in iOS Simulator
+# works only for Mac with iOS Simulator version 5.0 >
+
+# 1.  Start iOS Simulator, and visit the site
+# 2.  Execute the script and it will open up Safari with the Web Developer tools
+
+
+#####
+# find the process for the Mobile Safari
+#####
+MS_PID=$(ps x | grep "MobileSafari" | grep -v grep | awk '{ print $1 }')
+
+if [ "$MS_PID" == "" ]; then
+  # Mobile Safari not found - then exit
+  echo "Please start iOS Simulator with Mobile Safari opened the page you want to debug"
+  exit 1;
+else
+  # inject the code to the Mobile Safari process
+  cat <<EOM | gdb
+  attach $MS_PID
+  p (void *)[WebView _enableRemoteInspector]
+  detach
+EOM
+
+fi
+
+#####
+# get the remoting url and open up safari in developer tools
+#####
+RemotingURL=$(lsof -i TCP -a -p $MS_PID -P | awk 'NR==2{print $9}')
+
+echo Opening Developer Console $RemotingURL
+
+open "http://$RemotingURL"
