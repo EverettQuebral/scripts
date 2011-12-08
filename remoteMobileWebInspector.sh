4b4a0c8c5465eefabd59f8e780b56eb466ee1497
Everett Quebral <equebral@paypal.com>
2011-12-08 14:17:28 -0800

    adding parameter to support native uiwebview debugging

--- remoteMobileWebInspector.sh
+++ remoteMobileWebInspector.sh
@@ -9,24 +9,30 @@
 # 1.  Start iOS Simulator, and visit the site
 # 2.  Execute the script and it will open up Safari with the Web Developer tools
 
+if [ "$1" == "" ]; then
+	#####
+	# find the process for the Mobile Safari
+	#####
+	MS_PID=$(ps x | grep "MobileSafari" | grep -v grep | awk '{ print $1 }')
 
-#####
-# find the process for the Mobile Safari
-#####
-MS_PID=$(ps x | grep "MobileSafari" | grep -v grep | awk '{ print $1 }')
-
-if [ "$MS_PID" == "" ]; then
-  # Mobile Safari not found - then exit
-  echo "Please start iOS Simulator with Mobile Safari opened the page you want to debug"
-  exit 1;
-else
-  # inject the code to the Mobile Safari process
-  cat <<EOM | gdb
-  attach $MS_PID
-  p (void *)[WebView _enableRemoteInspector]
-  detach
+	if [ "$MS_PID" == "" ]; then
+	  # Mobile Safari not found - then exit
+	  echo "Please start iOS Simulator with Mobile Safari opened the page you want to debug"
+	  exit 1;
+	else
+	  # inject the code to the Mobile Safari process
+	  cat <<EOM | gdb
+	  attach $MS_PID
+	  p (void *)[WebView _enableRemoteInspector]
+	  detach
 EOM
 
+	fi
+
+else
+	echo "Finding Process ID for $1"
+	MS_PID=$(ps x | grep $1 | grep -v grep | awk 'NR==1{ print $1 }')
+	echo $MS_PID
 fi
 
 #####
